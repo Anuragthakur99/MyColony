38024cd80f0a9e49d760329059e23b3c102b7322
Anurag Thakur <anurag1321.be22@chitkara.edu.in>
2025-09-07 22:24:16 +0530

    new

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,8 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job"
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
-export const COLONY_API_END_POINT="http://localhost:8000/api/v1/colony"
-export const SERVICE_API_END_POINT="http://localhost:8000/api/v1/service"
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1";
+
+export const USER_API_END_POINT = `${API_BASE_URL}/user`;
+export const JOB_API_END_POINT = `${API_BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${API_BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${API_BASE_URL}/company`;
+export const COLONY_API_END_POINT = `${API_BASE_URL}/colony`;
+export const SERVICE_API_END_POINT = `${API_BASE_URL}/service`;
